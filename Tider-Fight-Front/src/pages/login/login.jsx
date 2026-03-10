4e90fa6b136968d2d7224b0e7fdc411abb4e89a9
jake7038 <rafaelcostatricolor@gmail.com>
2026-03-10 17:12:27 -0300

    feat: tela de login

---
+++ Tider-Fight-Front/src/pages/login/login.jsx
@@ -0,0 +1,18 @@
+import './login.css'
+ export function Login(){
+    return(
+        <div className="login-container">
+      <div className="overlay">
+        <div className="content">
+          
+            <img src="src/assets/Fighting_spirit_in_flames-removebg-preview 1.png" alt="Tinder da Luta" className="logo"/>
+
+          <div className="buttons">
+            <button className="btn primary">Entre na Arena</button>
+            <button className="btn secondary">Cadastre Agora</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
