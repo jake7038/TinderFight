4e49dec972f5bc9db6463670dd1ab7defc22225c
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2026-03-16 23:26:23 -0300

    feat: tela principal

--- Tider-Fight-Front/src/pages/login/login.jsx
+++ Tider-Fight-Front/src/pages/login/login.jsx
@@ -3,25 +3,26 @@ import { useNavigate } from 'react-router-dom'
 import './login.css'
 
 export function Login(){
-  const nav = useNavigate()
+    const nav = useNavigate()
 
-  const goHome = () => {
+    const goHome = () => {
     nav("/lutadores")
-  }
+    }
   
+
     return(
         <div className="login-container">
-      <div className="overlay">
-        <div className="content">
-          
-            <img src="src/assets/Fighting_spirit_in_flames-removebg-preview 1.png" alt="Tinder da Luta" className="logo"/>
+            <div className="overlay">
+                <div className="content">
 
-          <div className="buttons">
-            <button className="btn primary" onClick={goHome} >Entre na Arena</button>
-            <button className="btn secondary">Cadastre Agora</button>
-          </div>
+                    <img src="src/assets/Fighting_spirit_in_flames-removebg-preview 1.png" alt="Tinder da Luta" className="logo"/>
+
+                    <div className="buttons">
+                        <button className="btn primary" onClick={goHome} >Entre na Arena</button>
+                        <button className="btn secondary">Cadastre Agora</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
     )
 }
--- Tider-Fight-Front/src/pages/principal/principal.jsx
+++ Tider-Fight-Front/src/pages/principal/principal.jsx
@@ -1,13 +1,69 @@
 import "./principal.css"
 export function Principal() {
-    return(
-        <div>
-            <header className="header">
-                <div> oi</div>
-                <img src="src/assets/Choose your fighter in flames 1.png" alt="" />
-                <div> oi</div>
-            </header>
 
+    const modalidade = ['Jiu Jitsu', 'MMA']
+
+    return(
+        <div className="principal">
+            <header className="header">
+                <img src="/User.png" alt="" />
+                <img className="imagem_header" src="src/assets/Choose your fighter in flames 1.png" alt="" />
+                <img src="/Map.png" alt="" />
+            </header>
+            <div className="card">
+                    <h2 className="name">Charles do Bronx</h2>
+                    <div className="place_fighter">
+                        <img src="/pontogps.png" alt="" />
+                        <p>Guarujá, SP</p>
+                        <div className="category">
+                            {modalidade.map(mod => (
+                            <p key={mod}>{mod}</p>
+                            ))}
+                        </div>
+                    </div>
+                    
+            </div>
+            
+            <div className="choice">
+                    <div className="nop">
+                        <div className="circle">
+                            <img src="/x.png" alt="" />
+                        </div>
+                    </div>
+
+                    <div className="yes">
+                        <div className="circle">
+                            <img src="/mira.png" alt="" />
+                        </div>
+                    </div>
+            </div>
+            
+            <footer>
+                <div>
+                    
+                    <div>
+                        <img src="/glove.png" alt="" />
+                        <p>Swap</p>
+                    </div>
+                    
+                    <div>
+                        <img src="/chat.png" alt="" />
+                        <p>Chat</p>
+                    </div>
+                    
+                    <div>
+                        <img src="/lutador.png" alt="" />
+                        <p>Perfil do Lutador</p>
+                    </div>
+
+                    <div>
+                        <img src="/historico.png" alt="" />
+                        <p>Histórico</p>
+                    
+                    </div>
+                
+                </div>
+            </footer>
 
         </div>
 
