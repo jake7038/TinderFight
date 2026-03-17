777c737bed7ddafc7f0c3ede994fb300230c9b9d
jake7038 <rafaelcostatricolor@gmail.com>
2026-03-16 22:02:32 -0300

    feat: react router

--- Tider-Fight-Front/src/pages/login/login.jsx
+++ Tider-Fight-Front/src/pages/login/login.jsx
@@ -1,5 +1,14 @@
+import { useNavigate } from 'react-router-dom'
+
 import './login.css'
- export function Login(){
+
+export function Login(){
+  const nav = useNavigate()
+
+  const goHome = () => {
+    nav("/lutadores")
+  }
+  
     return(
         <div className="login-container">
       <div className="overlay">
@@ -8,7 +17,7 @@ import './login.css'
             <img src="src/assets/Fighting_spirit_in_flames-removebg-preview 1.png" alt="Tinder da Luta" className="logo"/>
 
           <div className="buttons">
-            <button className="btn primary">Entre na Arena</button>
+            <button className="btn primary" onClick={goHome} >Entre na Arena</button>
             <button className="btn secondary">Cadastre Agora</button>
           </div>
         </div>
---
+++ Tider-Fight-Front/src/pages/principal/principal.jsx
@@ -0,0 +1,16 @@
+import "./principal.css"
+export function Principal() {
+    return(
+        <div>
+            <header className="header">
+                <div> oi</div>
+                <img src="src/assets/Choose your fighter in flames 1.png" alt="" />
+                <div> oi</div>
+            </header>
+
+
+        </div>
+
+
+    )
+}
