8eb69a8d2690bb3c58c6726000c8da541ce21020
jake7038 <rafaelcostatricolor@gmail.com>
2026-03-17 17:53:11 -0300

    feat: desing da tela modelo de chat

--- Tider-Fight-Front/src/main.jsx
+++ Tider-Fight-Front/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import {Login} from './pages/login/login'
 import { Principal } from './pages/principal/principal'
+import Chat from './pages/chat/chat'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/lutadores" element={<Principal />} />
+        <Route path="/chat" element={<Chat/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
