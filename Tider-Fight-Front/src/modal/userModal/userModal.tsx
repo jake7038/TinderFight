f056d9b735fa18a3b6aa3ffb7b8a7e04c4dd3530
Rafael do Amaral Costa <rafaelcostatricolor@gmail.com>
2026-04-14 07:35:13 -0300

    fix: small changes in desing

--- Tider-Fight-Front/src/modal/userModal/userModal.tsx
+++ Tider-Fight-Front/src/modal/userModal/userModal.tsx
@@ -22,7 +22,7 @@ interface SettingsModalProps {
 }
 
 const ALL_MODALITIES = ["MMA", "Jiu Jitsu", "Muay Thai", "Boxe", "Wrestling", "Judô", "Capoeira"];
-const MY_MODALITIES = ["MMA", "Jiu Jitsu", "Muay Thai", "Boxe", "Wrestling", "Judô"];
+const MY_MODALITIES = ["MMA", "Jiu Jitsu", "Muay Thai", "Boxe", "Wrestling", "Judô", "Capoeira"];
 
 const DEFAULT_FIGHTER: FighterPreferences = {
     distance: 100,
@@ -33,7 +33,7 @@ const DEFAULT_FIGHTER: FighterPreferences = {
 
 
 
-const UserModal: React.FC<SettingsModalProps> = ({ isOpen, onClose }) => {
+const UserModal = ({ isOpen, onClose }: SettingsModalProps) => {
     const dispatch = useAppDispatch();
     const nav = useNavigate();
     const usuario = useAppSelector(state => state.usuario);
