26322ca54b9b6d4d8415c18cce279f2daa7c4201
Gabriel <gabrielfortinim@gmail.com>
2026-05-14 08:07:23 -0300

    add: configuração do bd

--- TinderFight-Back/knexfile.js
+++ TinderFight-Back/knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -7,10 +8,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: "127.0.0.1",
-      user: "postgres",
-      password: "",
-      database: "tinder_fight_db",
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: "./src/database/migrations",
